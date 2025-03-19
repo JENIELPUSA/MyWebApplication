a9951a6ca1b33987190d122c9631e419cdea7087
Jeniel Pusa <jeniel12300@gmail.com>
2025-03-19 13:18:36 +0800

    add based in vite

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/EquipmentMaintenance/", // Adjust ito base sa repository name mo
   plugins: [react()],
   server: {
     proxy: {
